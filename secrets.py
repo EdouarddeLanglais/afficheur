c0dde9194211026b22d982f9571e9033a15eb972
EdouarddeLanglais <98285731+EdouarddeLanglais@users.noreply.github.com>
2022-01-23 22:43:27 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,6 +1,3 @@
-# This file is where you keep secret settings, passwords, and tokens!
-# If you put them in the code you risk committing that info or sharing it
-
 secrets = {
     'ssid' : 'SFR-be88',
     'password' : 'DUIK4CVMPDU4',
